3a378d25263232aa4a73e2c57bc59667d766aafb
MateusLopes <mateus20.lopes02@gmail.com>
2023-04-07 16:39:29 -0300

    feat: adicionando rotas no navigation

--- src/components/Main/Navgation/Navgation.jsx
+++ src/components/Main/Navgation/Navgation.jsx
@@ -7,21 +7,19 @@ const Navgation = () => {
       <div className="wrapper">
         <ul className="naviagation">
           <li className="btn-a">
-            <a href="#o-que-e-?">O que é?</a>
+            <a href="#o-que-e">O que é?</a>
           </li>
           <li className="btn-a">
-            <a href="/beneficios-do-super-pós-obra?">
-              Benefícios do Super Pós Obra?
-            </a>
+            <a href="#beneficios">Benefícios do Super Pós Obra?</a>
           </li>
           <li className="btn-a">
-            <a href="/">Como usar?</a>
+            <a href="#como-usar">Como usar?</a>
           </li>
           <li className="btn-a">
-            <a href="/">Descrição</a>
+            <a href="#descricao">Descrição</a>
           </li>
           <li className="btn-a">
-            <a href="/">Quem é a Policlean Oirad?</a>
+            <a href="#quem-e-a-policlean-oirad">Quem é a Policlean Oirad?</a>
           </li>
         </ul>
       </div>
