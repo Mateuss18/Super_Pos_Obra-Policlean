1caf30182c6b1253cff4cd95155d165a98904208
MateusLopes <mateus20.lopes02@gmail.com>
2023-04-07 10:21:42 -0300

    feat: componente footer

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import FooterTop from './FooterTop/FooterTop'
+import FooterBottom from './FooterBottom/FooterBottom'
+
+const Footer = () => {
+  return (
+    <footer>
+      <FooterTop />
+      <FooterBottom />
+    </footer>
+  )
+}
+
+export default Footer
