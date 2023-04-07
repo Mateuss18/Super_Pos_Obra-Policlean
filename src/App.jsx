16b8ab77508704543b0782f248c1f776fd84d52d
MateusLopes <mateus20.lopes02@gmail.com>
2023-04-07 09:49:45 -0300

    feat: importando componentes main e footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import Main from './components/Main/Main'
+import Footer from './components/Footer/Footer'
+import './app.scss'
 
 function App() {
   return (
     <>
       <Main />
+      <Footer />
     </>
   )
 }
