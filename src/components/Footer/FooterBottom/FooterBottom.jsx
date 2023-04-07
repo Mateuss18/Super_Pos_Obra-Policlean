35d167433c7a9a4a80c75d35938ce4efaa7b95c1
MateusLopes <mateus20.lopes02@gmail.com>
2023-04-07 10:20:35 -0300

    feat: componente footer bottom

---
+++ src/components/Footer/FooterBottom/FooterBottom.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import LogoAgencia from './assets/logo_agenciadelucca.png'
+import './footerBottom.scss'
+
+const FooterBottom = () => {
+  return (
+    <div className="footer-bottom">
+      <div className="wrapper">
+        <p>
+          COPYRIGHT POLICLEAN OIRAD QUÍMICA - 50090646000185 - 2022. TODOS OS
+          DIREITOS RESERVADOS
+        </p>
+        <img src={LogoAgencia} alt="" />
+      </div>
+    </div>
+  )
+}
+
+export default FooterBottom
