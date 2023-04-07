ee7384cfada14cc498199de1519daf61d05bb049
MateusLopes <mateus20.lopes02@gmail.com>
2023-04-07 10:20:06 -0300

    fix: corringindo importacao

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,10 +1,24 @@
 import React from 'react'
 import Product from './Product/Product'
+import Navgation from './Navgation/Navgation'
+import SectionWhatIts from './SectionWhatIts/SectionWhatIts'
+import SectionBenefits from './SectionBenefits/SectionBenefits'
+import SectionHowToUse from './SectionHowToUse/SectionHowToUse'
+import SectionDescription from './SectionDescription/SectionDescription'
+import SectionWhoIsPO from './SectionWhoIsPO/SectionWhoIsPO'
+import SectionExplanation from './SectionExplanation/SectionExplanation'
 
 const Main = () => {
   return (
     <main>
       <Product />
+      <Navgation />
+      <SectionWhatIts />
+      <SectionBenefits />
+      <SectionHowToUse />
+      <SectionDescription />
+      <SectionWhoIsPO />
+      <SectionExplanation />
     </main>
   )
 }
